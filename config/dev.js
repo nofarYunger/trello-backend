6749e1ada3dfbf0600f21687d802e66a9f3090fd
Yoni Segev <Yonisegev6@gmail.com>
2021-01-23 18:30:55 +0200

    merge

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'dbURL':  'mongodb+srv://chello:chello2021@cluster0.nchpz.mongodb.net/chello?retryWrites=true&w=majority',
- 
-}
+  dbURL:
+    "mongodb+srv://chello:chello2021@cluster0.nchpz.mongodb.net/chello?retryWrites=true&w=majority",
+};
